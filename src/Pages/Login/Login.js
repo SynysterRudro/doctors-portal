300fc954452fbaaab45bb060e8f72153a7bee184
SynysterRudro <rudrosingh82@gmail.com>
2022-11-15 18:49:47 +0600

    login and signup page design completed

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from "react-hook-form";
+import { Link } from 'react-router-dom';
+
 
 const Login = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const handleLogin = data => {
+        console.log(data);
+    }
+
     return (
-        <div>
-            <h2>this is login</h2>
+        <div className='h-[800px] flex justify-center items-center'>
+            <div className='w-96 p-7'>
+                <h2 className='text-xl text-center mb-7'>Log in</h2>
+
+                <form onSubmit={handleSubmit(handleLogin)}>
+
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" className="input input-bordered w-full max-w-xs" {...register("email", { required: "email address is required" })} />
+                        {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" className="input input-bordered w-full max-w-xs" {...register("password", { required: "password address is required", minLength: { value: 6, message: 'password must be 6 characters or more' } })} />
+                        {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
+
+                        <label className="label">
+                            <span className="label-text">Forget password?</span>
+                        </label>
+
+                    </div>
+
+
+                    <input type="submit" className='btn btn-accent w-full' value='login' />
+                </form>
+
+                <p>New to Doctors Portal? <Link className='text-secondary' to='/signup'>Create new account</Link></p>
+
+                <div className="divider">OR</div>
+                <button className='uppercase btn btn-outline w-full'>Continue with google</button>
+            </div>
         </div>
     );
 };
---
+++ src/Pages/Signup/Signup.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+
+const Signup = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const handleSignUp = data => {
+        console.log(data);
+        console.log(errors);
+    }
+    return (
+
+        <div className='h-[800px] flex justify-center items-center'>
+            <div className='w-96 p-7'>
+                <h2 className='text-xl text-center mb-7'>Log in</h2>
+
+                <form onSubmit={handleSubmit(handleSignUp)}>
+
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" {...register("name", { required: 'name is required' })} className="input input-bordered w-full max-w-xs" />
+                        {errors.name && <p className='text-red-600'>{errors.name.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" {...register("email", { required: "email is required" })} className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p className='text-red-600'>{errors.email.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" {...register("password", { required: "Password is required", minLength: { value: 6, message: 'password must be 6 characters or more' } })} className="input input-bordered w-full max-w-xs" />
+                        {errors.password && <p className='text-red-600'>{errors.password.message} </p>}
+                    </div>
+
+
+                    <input type="submit" className='btn btn-accent w-full mt-4' value='signup' />
+                </form>
+
+                <p>Already have an account? <Link className='text-secondary' to='/login'>Login</Link></p>
+
+                <div className="divider">OR</div>
+                <button className='uppercase btn btn-outline w-full'>Continue with google</button>
+            </div>
+        </div>
+    );
+};
+
+export default Signup;
