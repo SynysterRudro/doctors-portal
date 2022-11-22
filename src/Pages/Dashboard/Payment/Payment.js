8a2655355ad547f17e6faa21abb1c9a6ef13f353
SynysterRudro <rudrosingh82@gmail.com>
2022-11-22 20:25:05 +0600

    error element set korsi

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Payment = () => {
-    const { data } = useLoaderData();
+    const booking = useLoaderData();
+    const { treatment, price, slot, appointmentDate } = booking;
+    // console.log(booking);
     return (
         <div>
-            <h3 className="text-3xl">Payment</h3>
+            <h3 className="text-3xl mb-2">Payment for - <span className='text-4xl font-bold text-primary'>{treatment}</span></h3>
+            <p className="text-xl">Please pay <strong>${price}</strong> for your appointment {appointmentDate} at {slot}</p>
         </div>
     );
 };
